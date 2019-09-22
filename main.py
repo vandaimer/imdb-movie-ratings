877105e9288baf1625b55f205010a80bf0b3b206
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-21 22:17:49 -0300

    Add number_results click options

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ logger = logging.getLogger()
 @click.command()
 @click.option('-l', '--load-data', required=True, default=False, help='Load data from internet')
 @click.option('-w', '--search-word', required=True, type=str, help='Word to search')
-def main(load_data, search_word):
+@click.option('-n', '--number-results', default=100, type=int, help='Number of results')
+def main(load_data, search_word, number_results):
     TITLE_BASIC_FILE = os.getenv('TITLE_BASIC_FILE', 'title.basics.tsv')
     TITLE_RATINGS_FILE = os.getenv('TITLE_BASIC_FILE', 'title.ratings.tsv')
     BASE_URL = os.getenv('IMDB_BASE_URL', 'https://datasets.imdbws.com')
@@ -44,9 +45,9 @@ def main(load_data, search_word):
     logger.info(f'Searching for {search_word} ...')
     movies = imdb.search(search_word)
 
-    logger.info('Is going to show you the first 100 first result...')
+    logger.info(f'Is going to show you the first {number_results} first result...')
     headers = ('Title', 'Rating')
-    print(tabulate(list(map(lambda x: [x[1]['primaryTitle'], x[1]['rating']], movies[:100])), headers=headers))
+    print(tabulate(list(map(lambda x: [x[1]['primaryTitle'], x[1]['rating']], movies[:number_results])), headers=headers))
 
 
 if __name__ == '__main__':
